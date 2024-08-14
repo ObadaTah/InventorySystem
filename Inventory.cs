8459348719c8a80328917764b220ea1720ad18aa
ObadaTah <obadatahboub20@gmail.com>
2024-08-14 18:44:29 +0300

    Added Inventory class with its methods

---
+++ Inventory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem
+{
+    class Inventory
+    {
+        List<Product> Products { get; set; }
+
+        public Inventory()
+        {
+            Products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            Products.Remove(product);
+        }
+
+        public Product GetProduct(string name)
+        {
+            return Products.Find(product => product.Name == name);
+        }
+
+        public void PrintInventory()
+        {
+            foreach (Product product in Products)
+            {
+                product.PrintProduct();
+            }
+        }
+    }
+}
